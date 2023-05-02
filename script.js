d76844d7e630a73f54535443fd7d4460f48ff33b
martingabetta1 <105307200+martingabetta1@users.noreply.github.com>
2023-05-01 22:13:21 -0300

    Improve experience
    Add counters for win, loss and tie
    Show computer choice

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-// Rock papers scissors game
+// rock papers scissors game
 
 let winCount = 0;
 let lossCount = 0;
 let tieCount = 0;
-const scoreCount = document.querySelector("#counter");
-scoreCount.textContent = `${winCount} - ${lossCount} (${tieCount})`;
+const winCounthtml = document.querySelector("#win-counter");
+const lossCounthtml = document.querySelector("#loss-counter");
+const tieCounthtml = document.querySelector("#tie-counter");
+winCounthtml.textContent = `${winCount}`;
+lossCounthtml.textContent = `${lossCount}`;
+tieCounthtml.textContent = `${tieCount}`;
 
 const btnRock = document.querySelector("#rock");
 const btnPaper = document.querySelector("#paper");
 const btnScissors = document.querySelector("#scissors");
+const gameOutput = document.querySelector("#game-output");
 
-btnRock.onclick = () => playRound("Rock", getComputerChoice());
-btnPaper.onclick = () => playRound("Paper", getComputerChoice());
-btnScissors.onclick = () => playRound("Scissors", getComputerChoice());
+btnRock.onclick = () => playRound("rock", getComputerChoice());
+btnPaper.onclick = () => playRound("paper", getComputerChoice());
+btnScissors.onclick = () => playRound("scissors", getComputerChoice());
 
 // Computer random choice
 function getComputerChoice() {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ["rock", "paper", "scissors"];
   const choice = Math.floor(Math.random() * 3);
-  console.log(`The computer played ${options[choice]}`);
   return options[choice];
 }
 
-function win() {
+function win(playerChoice, computerChoice) {
+  gameOutput.textContent = `You won! ${playerChoice} beats ${computerChoice}.`;
   winCount++;
-  scoreCount.textContent = `${winCount} - ${lossCount} (${tieCount})`;
+  winCounthtml.textContent = `${winCount}`;
 }
 
-function fail() {
+function fail(playerChoice, computerChoice) {
+  gameOutput.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
   lossCount++;
-  scoreCount.textContent = `${winCount} - ${lossCount} (${tieCount})`;
+  lossCounthtml.textContent = `${lossCount}`;
 }
 
 function tie() {
+  gameOutput.textContent = "It's a tie!";
   tieCount++;
-  scoreCount.textContent = `${winCount} - ${lossCount} (${tieCount})`;
+  tieCounthtml.textContent = `${tieCount}`;
 }
 
 // Play a Round
 function playRound(player, computer) {
-  if(player === computer){
+  if (player === computer) {
     return tie();
   }
-  // Rock beats scissors and loses to paper
-  if (player === "Rock" && computer === "Scissors") {
-    return win();
-  } else if (player === "Rock" && computer === "Paper") {
-    return fail();
-  }
-  // Paper beats rock and loses to scissors
-  else if (player === "Paper" && computer === "Rock") {
-    return win();
-  } else if (player === "Paper" && computer === "Scissors") {
-    return fail();
-  }
-  // Scissors beats paper and loses to rock
-  else if (player === "Scissors" && computer === "Paper") {
+  // rock beats scissors and loses to paper
+  if (player === "rock" && computer === "scissors") {
     return win(player, computer);
-  } else if (player === "Scissors" && computer === "Rock") {
+  } else if (player === "rock" && computer === "paper") {
+    return fail(player, computer);
+  }
+  // paper beats rock and loses to scissors
+  else if (player === "paper" && computer === "rock") {
+    return win(player, computer);
+  } else if (player === "paper" && computer === "scissors") {
+    return fail(player, computer);
+  }
+  // scissors beats paper and loses to rock
+  else if (player === "scissors" && computer === "paper") {
+    return win(player, computer);
+  } else if (player === "scissors" && computer === "rock") {
     return fail(player, computer);
   }
 }
